dfd4760f891da724e68c333d7d44a0510aaee070
SHYAM HARODE <100039836+ShyamHarode@users.noreply.github.com>
2022-09-17 16:01:10 +0530

    Update AllShows.js

--- src/AllShows.js
+++ src/AllShows.js
@@ -1,17 +1,20 @@
 import "./styles.css";
 import ShowBox from "./ShowBox";
-import ActorBox from "./ActorBox";
 
 const AllShows = (props) => {
   return (
     <div className="showList">
-      {props.showList.map((showDetails, idx) => {
-        return props.option === "Show" ? (
-          <ShowBox key={idx} showDetails={showDetails} />
-        ) : (
-          <ActorBox key={idx} showDetails={showDetails} />
-        );
-      })}
+      {props.showList.length > 0
+        ? props.showList.map((showDetails, idx) => {
+            if (props.option === "Show") {
+              return <ShowBox key={idx} show={showDetails.show} />;
+            } else if (props.option === "Actor") {
+              return <ShowBox key={idx} show={showDetails._embedded.show} />;
+            } else {
+              return <ShowBox key={idx} show={showDetails} />;
+            }
+          })
+        : ""}
     </div>
   );
 };
